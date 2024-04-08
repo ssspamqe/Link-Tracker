672e46ba433d74aa0a4143cbdb35b2f1de82659c
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-04-09 00:02:16 +0300

    Return bugged files on github

---
+++ scrapper/src/main/java/edu/java/configuration/beansconfiguration/CachingConfiguration.java
@@ -0,0 +1,9 @@
+package edu.java.configuration.beansconfiguration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CachingConfiguration {
+}

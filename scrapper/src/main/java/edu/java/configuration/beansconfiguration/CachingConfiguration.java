2ceaf1bd7a70d592174fe81055d33780f83816f2
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-04-09 00:01:14 +0300

    Delete bugged files on github

--- scrapper/src/main/java/edu/java/configuration/beansconfiguration/CachingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package edu.java.configuration.beansconfiguration;
-
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
-
-@EnableCaching
-@Configuration
-public class CachingConfiguration {
-}

ec5ef213a95581294b7b360179f0381713774d74
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-04-18 13:53:25 +0300

    Add missing GitHub repository activies checker

---
+++ scrapper/src/main/java/edu/java/linkupdatescheduler/linkupdatescheckers/singleupdatecheckers/gitHub/GitHubRepositoryActivitiesUpdateChecker.java
@@ -0,0 +1,40 @@
+package edu.java.linkupdatescheduler.linkupdatescheckers.singleupdatecheckers.gitHub;
+
+import edu.java.data.dto.GitHubRepository;
+import edu.java.telegrambotconnection.dto.linkupdatedto.LinkUpdateType;
+import java.util.Set;
+import java.util.stream.Collectors;
+import edu.java.webclients.gitHub.GitHubClient;
+import edu.java.webclients.gitHub.dto.GitHubRepositoryActivityBody;
+import edu.java.webclients.gitHub.dto.GitHubRepositoryBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GitHubRepositoryActivitiesUpdateChecker implements GitHubRepositorySingleUpdateChecker {
+
+    private final GitHubClient gitHubClient;
+
+    @Override
+    public LinkUpdateType getType() {
+        return LinkUpdateType.GIT_HUB_REPOSITORY_ACTIVITIES;
+    }
+
+    @Override
+    public boolean hasUpdate(GitHubRepository oldState, GitHubRepositoryBody newState) {
+        Set<Long> oldActivities = oldState.getActivitiesIds();
+        Set<Long> newActivities = fetchActivitiesIds(newState.name(), newState.owner().login());
+
+        return !oldActivities.equals(newActivities);
+    }
+
+    private Set<Long> fetchActivitiesIds(String name, String owner) {
+        return gitHubClient
+            .fetchRepositoryActivitiesByRepositoryNameAndOwner(name, owner)
+            .stream()
+            .map(GitHubRepositoryActivityBody::id)
+            .collect(Collectors.toSet());
+    }
+
+}
